3dee22b6b6ebaffddd8a0668dc52e3f74f2f693c
Bohdan <boghdan.shimko@gmail.com>
2024-01-20 01:31:22 +0200

    add: manifest, favicon and screenshot

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+    })
+  ],
 })
